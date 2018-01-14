36c4bb6c27006e43f93c9e2a7628dfa1dfe742c4
asita88 <342122003@qq.com>
2018-01-14 12:44:03 +0800

    fadsf
    adfads

--- cmd.bat
+++ cmd.bat
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-
-#python cpa700_setup.py build_ext --inplace
-=======
->>>>>>> bdd60dc2ac0d9e6627e4e28be046d0fa809a15bb
 
 python cpa700_setup.py build_ext --inplace
 
 python htd188_setup.py build_ext --inplace
 
-
-
-
-#python sbmsc_setup.py build_ext --inplace
+python sbmsc_setup.py build_ext --inplace
 
 
 pyinstaller -F  cpa700_main.py
