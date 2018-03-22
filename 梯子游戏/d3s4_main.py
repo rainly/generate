dc504509e7807e4e9ae77f6da7924a22e580c2d2
wengyn <wengyn@yealink.com>
2018-03-22 19:31:46 +0800

    更新
    Change-Id: I068536a4c47898d947ad75ebd9548ae46091b2ee

--- 梯子游戏/d3s4_main.py
+++ 梯子游戏/d3s4_main.py
@@ -2,7 +2,28 @@
 ##Author：哈士奇说喵
 #pyinstaller
 #梯子游戏
+    
+import re
+import urllib
+import urllib.request
+import sys
+import io
+import json
+import time
+import datetime
+import http.cookiejar
+import json
+import pymysql.cursors
+import ssl
+#import sqlite3
+import configparser
+import random
 
+from bs4 import BeautifulSoup  # 引入beautifulsoup 解析html事半功倍
+from collections import deque
+from selenium import webdriver
+from selenium.common.exceptions import *
+from selenium.common.exceptions import NoSuchElementException
 from tkinter import *
 from tkinter import *
 from tkinter import ttk
@@ -10,22 +31,10 @@ from tkinter import scrolledtext
 from tkinter import Menu
 from tkinter import Spinbox
 from tkinter import messagebox as mBox
+
 import tkinter.messagebox as messagebox
 import tkinter as tk
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-import sqlite3
 import threading  
-import time
-
-import urllib
-import urllib.request
-import urllib.response
-import urllib.error
-import http.cookiejar
-import configparser
-import re
-import ssl
 from d3s4 import *
 ssl._create_default_https_context = ssl._create_unverified_context
 
